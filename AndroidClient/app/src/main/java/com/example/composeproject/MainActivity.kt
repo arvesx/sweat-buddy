487d78a5e50c6c8ad4d2be9eccbbddddd638ec16
Ares Velegrakis <arisveleg@gmail.com>
2023-05-23 04:48:44 +0300

    google maps setup, started impl of routes creation page

--- AndroidClient/app/src/main/java/com/example/composeproject/MainActivity.kt
+++ AndroidClient/app/src/main/java/com/example/composeproject/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RoutesScreen()
+            HomeScreen()
         }
     }
 }
---
+++ AndroidClient/app/src/main/java/com/example/composeproject/NewRouteActivity.kt
@@ -0,0 +1,167 @@
+package com.example.composeproject
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeproject.ui.theme.Blue1
+import com.example.composeproject.ui.theme.Blue2
+import com.example.composeproject.ui.theme.ManropeFamily
+import com.example.composeproject.ui.theme.White1
+import com.example.composeproject.ui.theme.WhiteBlue1
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun NewRouteScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colorStops = arrayOf(
+                        0.0f to Blue2,
+                        1f to Blue1,
+                    ),
+                )
+            )
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.clip(CircleShape)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.left),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(38.dp)
+                        )
+                    }
+                    Text(
+                        text = "Routes", color = colorResource(id = R.color.white),
+                        fontFamily = ManropeFamily,
+                        fontSize = 18.sp,
+                    )
+                    TextButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.clip(CircleShape)
+                    ) {
+                        Text(
+                            text = "Create", color = Color.White, fontFamily = ManropeFamily,
+                            fontSize = 18.sp, fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+
+            }
+            Spacer(modifier = Modifier.height(130.dp))
+            Card(
+                shape = RoundedCornerShape(topStart = 35.dp, topEnd = 35.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+
+            ) {
+
+                MapScreen()
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Column() {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                )
+                Card(
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 5.dp,
+                        hoveredElevation = 10.dp
+                    ), modifier = Modifier
+                        .fillMaxWidth()
+                        .height(180.dp)
+                ) {
+                }
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun MapScreen() {
+    val singapore = LatLng(1.35, 103.87)
+    val singaporeState = MarkerState(position = singapore)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = singaporeState,
+            title = "Marker in Singapore"
+        )
+        Marker(
+            state = MarkerState(position = LatLng(37.979230, 23.725860)),
+            title = "Marker in Singapore"
+        )
+    }
+}
