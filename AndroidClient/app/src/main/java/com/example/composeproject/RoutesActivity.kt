692bf96fed6a5c44c0b71a172a22e212f067a1fd
Ares Velegrakis <arisveleg@gmail.com>
2023-05-28 04:02:32 +0300

    worked on the screen ui of a specific route

--- AndroidClient/app/src/main/java/com/example/composeproject/RoutesActivity.kt
+++ AndroidClient/app/src/main/java/com/example/composeproject/RoutesActivity.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,10 +21,8 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -36,11 +32,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composeproject.data.DataProvider
@@ -69,11 +65,11 @@ fun RoutesScreen() {
                 )
             )
     ) {
-        TopBar()
+        UpperSection(120.dp)
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(40.dp),
+                .padding(start = 40.dp, end = 40.dp, top=40.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(
@@ -154,15 +150,17 @@ fun RouteCard(route: RouteInfo) {
                     ) {
                         var iconType: Painter = painterResource(id = R.drawable.mountain)
                         var stringType: String = "UNKNOWN"
-                        when(route.type) {
+                        when (route.type) {
                             0 -> {
                                 iconType = painterResource(id = R.drawable.mountain)
                                 stringType = "HIKING"
                             }
+
                             1 -> {
                                 iconType = painterResource(id = R.drawable.path)
                                 stringType = "WALK"
                             }
+
                             2 -> {
                                 iconType = painterResource(id = R.drawable.runningshoe)
                                 stringType = "RUN"
@@ -319,11 +317,11 @@ fun TopCard(totalNumberOfRoutes: Int, totalKilometers: Float) {
 }
 
 @Composable
-fun TopBar() {
+fun UpperSection(height: Dp) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(120.dp)
+            .height(height)
             .background(
                 Brush.verticalGradient(
                     colorStops = arrayOf(
