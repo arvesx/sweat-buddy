6bccf7046128a499186ce9bd123c1e299133c8c7
Ares Velegrakis <arisveleg@gmail.com>
2023-06-15 21:28:56 +0300

    code added, features implemented

---
+++ Dependencies/src/main/java/dependencies/user/Route.java
@@ -0,0 +1,14 @@
+package dependencies.user;
+
+public class Route {
+    public String routeName;
+    public int points;
+
+    public double totalDistanceInKm;
+    public double totalElevationInM;
+    public double averageSpeedInKmH;
+    public double totalTimeInMinutes;
+
+    public int routeType;
+
+}
